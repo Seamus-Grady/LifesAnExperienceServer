5e205f36fd1f22df915c4d5e9286717b2a2f4e6d
Seamus-Grady <u0995999@utah.edu>
2021-02-20 19:28:06 -0700

    Added the port number the server is running on when the server starts

--- app.js
+++ app.js
@@ -399,5 +399,5 @@ const saveArrayAsFile =  (arrayBuffer, filePath)=> {
 
 const port = process.env.port || 3000;
 app.listen(port, () => {
-    console.log("Server started");
+    console.log("Server started on port " + port);
 });
